4f11e1df0c274cc3cc84998533b09fdf28f6b66e
Temmittaton <65850687+Temmittaton@users.noreply.github.com>
2022-11-11 22:47:49 +0100

    Fixed Jules's shit
    - Simplified wall script
    - Fixed errors

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -5,43 +5,31 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     public Material[] textureList;
-    public int lifeMax = 3;
-    public int life;
-    private int currentLife;
+    public int lifeMax, life = 3;
 
-    // Update is called once per frame
-    void Start()
-    {
-        int life = lifeMax;
+    void Start(){
         int currentLife = life;
     }
-    void TextureChange()
-    {
+
+    void TextureChange(){
         Material mat = textureList[lifeMax - life];
         gameObject.GetComponent<SpriteRenderer>().material = mat;
     }
-    void TakeDamage()
-    {
-        life -= 1;
-    }
-    private void Update()
-    {
-        if (life == 0)
-        {
+
+    void TakeDamage(){
+        if (life == 1){
             Destroy(gameObject, 0);
         }
-        if (life != currentLife) {
+        else {
+            life--;
             TextureChange();
-            currentLife = life;
         }
+        
     }
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if(col.tag == "projectile")
-        {
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.tag == "projectile"){
             TakeDamage();
         }
     }
-    
 }
